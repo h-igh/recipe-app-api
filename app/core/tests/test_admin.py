eef9547c12d7fd5450148b8e3fed58a75466b942
h-igh <igh.hou@gmail.com>
2023-11-12 20:43:07 -0500

    Added test admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
